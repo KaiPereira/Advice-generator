31918495c8d0ababb936d93883fced2febbe6fc1
KaiPereira <88850028+KaiPereira@users.noreply.github.com>
2022-11-23 16:05:30 -0800

    backup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from "react"
+import axios from "axios"
 
 function App() {
+  const [advice, changeAdvice] = React.useState()
+
+  React.useEffect(() => {
+    axios.get("https://api.adviceslip.com/advice")
+      .then(advice => changeAdvice(advice.data.slip))
+  }, [])
+
+  function changeAdviceFunction() {
+    axios.get("https://api.adviceslip.com/advice")
+      .then(advice => changeAdvice(advice.data.slip))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>{ advice &&
+      <div className="card">
+        <h1>ADVICE #{advice.id}</h1>
+        <p className="cardDescription">{advice.advice}</p>
+        <div className="cardDecoration">
+          <div className="cardDecorationLine"></div>
+          <i class="fa-solid fa-pause"></i>
+          <div className="cardDecorationLine"></div>
+        </div>
+        <button className="newAdviceButton" onClick={changeAdviceFunction}>
+          <i class="fa-solid fa-dice-four"></i>
+        </button>
+      </div>}
+    </main>
   );
 }
 
